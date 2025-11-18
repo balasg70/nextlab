7737f963bf48827992d69545a1783631dc29b211
balasg70 <56418977+balasg70@users.noreply.github.com>
2025-11-18 22:27:15 +0800

    Update run-jmeter-test.sh

--- scripts/run-jmeter-test.sh
+++ scripts/run-jmeter-test.sh
@@ -5,8 +5,9 @@ echo "==============================================="
 echo " Running JMeter distributed load test locally"
 echo "==============================================="
 
-echo "[1/3] Ensuring containers are running..."
+echo "Ensuring containers are running..."
 docker-compose up -d jmeter-master jmeter-slave1 jmeter-slave2
 
-echo "[2/3] Tail JMeter master output..."
+echo "Tail JMeter master output..."
 docker logs -f jmeter-master | tee jmeter-test.log
+
